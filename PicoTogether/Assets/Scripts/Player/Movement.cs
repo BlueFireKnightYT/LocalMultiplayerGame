43aca75be91b6e89803f1645c75a37b83fb55828
BlueFireKnightYT <vanmeerveldsGames@outlook.com>
2026-01-13 10:36:04 +0100

    Player half werkend

---
+++ PicoTogether/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpHeight;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheck;
+
+    float InputX;
+
+
+    void Update()
+    {
+        rb.linearVelocityX = InputX * speed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        InputX = context.ReadValue<Vector2>().x;
+    }
+    public bool isGrounded()
+    {
+        var hits = Physics2D.OverlapCapsuleAll
+        (
+            groundCheck.position,
+            new Vector2(1f, 0.5f),
+            CapsuleDirection2D.Horizontal,
+            0f,
+            groundLayer
+        );
+
+        return hits.Length > 0;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded() == true)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpHeight);
+        }
+    }
+}
