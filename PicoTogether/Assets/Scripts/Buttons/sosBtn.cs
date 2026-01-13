f03d51b113b6a5b142bc30abab23682ea02cd354
Voxin <mark@digitalice.nl>
2026-01-13 10:17:24 +0100

    TMP & Start layout
    btn funcs, start, Start Scherm

---
+++ PicoTogether/Assets/Scripts/Buttons/sosBtn.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using SceneManager = UnityEngine.SceneManagement.SceneManager;
+
+public class sosBtn : MonoBehaviour
+{
+    public void sos()
+    {
+        Debug.Log("not banana = skill issue");
+    }
+}
