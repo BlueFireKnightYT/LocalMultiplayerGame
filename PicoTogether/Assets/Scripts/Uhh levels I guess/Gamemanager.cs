8ee995c00e925ebc27e885c713c8e3ed94d708e1
ragort419-lab <runnengames@gmail.com>
2026-01-13 14:00:50 +0100

    Add door and key mechanics to level
    Introduces Door and Key scripts to handle player interactions for unlocking and progressing to the next level. Updates Gamemanager to instantiate the key when required buttons are pressed and to load the next scene when both door and key conditions are met. Scene and prefab files updated to include new components.

--- PicoTogether/Assets/Scripts/Uhh levels I guess/Gamemanager.cs
+++ PicoTogether/Assets/Scripts/Uhh levels I guess/Gamemanager.cs
@@ -1,9 +1,15 @@
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Gamemanager : MonoBehaviour
 {
+    public GameObject pref_Key;
     public int pressedButtons = 0;
     public int neededButtons = 0;
+    public bool door = false;
+    public bool key = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,8 +21,12 @@ public class Gamemanager : MonoBehaviour
     {
         if (pressedButtons == neededButtons)
         {
-            Instantiate(gameObject);
+            Instantiate(pref_Key, new Vector2(-0.25f, -1.75f), Quaternion.identity);
         }
 
+        if(door == true && key == true && Input.GetKeyDown("e"))
+        {
+            SceneManager.LoadScene("Level2");
+        }
     }
 }
